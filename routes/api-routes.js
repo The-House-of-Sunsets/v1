91f128ed9477eef8dce3f152749896230cd9ad0e
hschaen <hschaen@gmail.com>
2020-04-15 16:52:51 -0700

    ok, think this is working now

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 var links = require('./links');
 /* GET home page. */
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
 router.get('/v1_0/links', function(req, res, next) {
     let newLinks = JSON.stringify(links);
+    console.log("ooo: " + newLinks );
     res.send(links);
     //   res.render('index', { title: 'Express' });
 });

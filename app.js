cf04fb83a4b753deb030f0b88e66d9ff3d3ae8f4
hschaen <hschaen@gmail.com>
2020-04-15 15:35:14 -0700

    fixing express engine issue

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api-routes');
-var userRouter = require('./routes/users');
 
 var app = express();
 
@@ -15,8 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', indexRouter);
 app.use('/api', apiRouter);
-app.use('/users', userRouter);
 
 module.exports = app;

c13751f2ca65b1fd47c893f86b46a9f5c43bcf4d
hschaen <hschaen@gmail.com>
2020-04-15 18:04:55 -0700

    Adding to git, seeing if that works pushing to heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var path = require('path');
 var apiRouter = require('./routes/api-routes');
 
 var app = express();
@@ -14,6 +14,9 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 app.use('/api', apiRouter);
 
 module.exports = app;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-
-/* GET home page. */
-router.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-module.exports = router;
